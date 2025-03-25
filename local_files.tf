f5870a60777a61c37b17c73f83d66112534d011b
arodriguez1996 <arodriguezv22@gmail.com>
2025-03-25 17:44:41 -0500

    other commands and lint

--- local_files.tf
+++ local_files.tf
@@ -1,5 +1,5 @@
 resource "local_file" "tasks" {
-    count = 3
-    content = "find a good job"
-    filename = "tasks-${random_string.sufix[count.index].id}.txt"
+  count    = 3
+  content  = "find a good job"
+  filename = "tasks-${random_string.sufix[count.index].id}.txt"
 }
--- random.tf
+++ random.tf
@@ -1,7 +1,7 @@
 resource "random_string" "sufix" {
-    count = 3
-    length = 8
-    special = false
-    upper = false
-    numeric = false
+  count   = 3
+  length  = 8
+  special = false
+  upper   = false
+  numeric = false
 }
