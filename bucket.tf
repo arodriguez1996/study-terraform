bf1bfec7ac16730ed33d3d270728563099d8c31e
arodriguez1996 <arodriguezv22@gmail.com>
2025-03-25 17:34:37 -0500

    first s3 creation

---
+++ bucket.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "tasks" {
+    count = 3
+    bucket = "tasks-${random_string.sufix[count.index].id}"    
+    tags = {
+        Owner = "deepmerge"
+        Environment = "Dev"
+    }
+}
--- terraform.tf
+++
@@ -1,4 +0,0 @@
-resource "local_file" "tasks" {
-    content = "find a good job \n buy a mirrow"
-    filename = "tasks.txt"
-}
