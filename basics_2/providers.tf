7fc1763c62ae4e4e5dae8d663cd6fe64d82df03e
arodriguez1996 <arodriguezv22@gmail.com>
2025-03-28 18:16:02 -0500

    vpc

---
+++ basics_2/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.92.0"
+    }
+  }
+
+  required_version = "1.11.2"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "aws" {
+  region = "us-east-2"
+  alias = "ohio"
+}
